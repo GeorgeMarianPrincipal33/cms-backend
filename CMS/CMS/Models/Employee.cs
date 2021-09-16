3d64bca984a89207ef325bdfec06bb4aeaf86844
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-09-16 15:38:43 +0300

    create employee data model

--- CMS/CMS/Models/Employee.cs
+++ CMS/CMS/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace CMS.Models
 {
     public class Employee
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public int Gender { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+        public string ProfileImage { get; set; }
+
     }
 }
